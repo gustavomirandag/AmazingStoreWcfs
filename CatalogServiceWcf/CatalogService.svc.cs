c5f0ee539ee92d9566dbbd803b206281485b7342
gustavomirandag <gustavomirandag@gmail.com>
2019-01-31 22:15:38 -0200

    Web Store using Windows Communication Foundation.

---
+++ CatalogServiceWcf/CatalogService.svc.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using CatalogServiceWcf.Models;
+
+namespace CatalogServiceWcf
+{
+    public class CatalogService : ICatalogService
+    {
+        //Ram Database
+        private readonly static List<CatalogProduct> _catalog =
+            new List<CatalogProduct>()
+            {
+                new CatalogProduct
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Bicicleta",
+                    MinPrice = 500
+                },
+                new CatalogProduct
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Notebook Gamer",
+                    MinPrice = 5000
+                },
+                new CatalogProduct
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "Kindle",
+                    MinPrice = 300
+                }
+            };
+
+        public IEnumerable<CatalogProduct> GetAllAvailableProducts()
+        {
+            return _catalog;
+        }
+    }
+}
---
+++ CatalogServiceWcf/Models/CatalogProduct.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CatalogServiceWcf.Models
+{
+    public class CatalogProduct
+    {
+        public Guid Id { get; set; }
+        public string ImageUrl { get; set; }
+        public string Title { get; set; }
+
+        public int StoresCount { get; set; }
+        public Decimal MinPrice { get; set; }
+
+    }
+}
